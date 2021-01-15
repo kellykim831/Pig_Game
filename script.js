bb804cc7c681e4d940e6820d27155814c877f1dc
Kelly Kim <kellykim408@gmail.com>
2021-01-14 22:29:46 -0800

    add starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContext = 0;
+score1El.textContext = 0;
+diceEl.classList.add('hidden');
