3a0efb226fba963e8e3711417e505eb439604b2e
Kelly Kim <kellykim408@gmail.com>
2021-01-21 20:12:50 -0800

    add new elements and dice functionality

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 'use strict';
 
+//Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContext = 0;
 score1El.textContext = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Rolling dice funtionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  //3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContext = currentScore;
+  } else {
+    //Switch to next player
+  }
+});
